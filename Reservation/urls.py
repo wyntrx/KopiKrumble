34a3c3b6957e97aa70e5238ab852627d082af8d0
Demigud <iabarsosa@gmail.com>
2022-04-01 17:45:08 +0800

    Dashboard Update

--- Reservation/urls.py
+++ Reservation/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    #path('admin/', include('admin_app.urls', namespace='admin_app')),
+    path('admin/', include('admin_app.urls', namespace='admin_app')),
     path('admin/', admin.site.urls), 
     
     path('', include('user_app.urls', namespace='user_app')),
